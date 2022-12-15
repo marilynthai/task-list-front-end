a5de52bcccb417acf4428ccc63147d2c9c37cf71
Marilyn Thai <marilynthai15@gmail.com>
2022-12-15 14:00:42 -0800

    Finished wave 2, lifting state.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,50 @@ const TASKS = [
 ];
 
 const App = () => {
+  const taskCopy = TASKS.map((task) => {
+    return { ...task };
+  });
+
+  const [tasks, setTasks] = useState(taskCopy);
+
+  const toggleComplete = (taskId) => {
+    const updatedTasks = [];
+    for (const task of tasks) {
+      if (task.id !== taskId) {
+        updatedTasks.push(task);
+      } else {
+        const newTask = { ...task, isComplete: !task.isComplete };
+        updatedTasks.push(newTask);
+      }
+    }
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = [];
+    for (const task of tasks) {
+      if (task.id !== taskId) {
+        updatedTasks.push(task);
+      }
+    }
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {
+            <TaskList
+              tasks={tasks}
+              toggleComplete={toggleComplete}
+              deleteTask={deleteTask}
+            />
+          }
+        </div>
       </main>
     </div>
   );
