d57dc45f126c235ee9bb7890a4a7006664c82a62
Marilyn Thai <marilynthai15@gmail.com>
2022-12-19 12:10:41 -0800

    Wave 3 complete, weird css things going on

--- src/App.js
+++ src/App.js
@@ -1,49 +1,93 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+// const taskCopy = TASKS.map((task) => {
+//   return { ...task };
+// });
 
 const App = () => {
-  const taskCopy = TASKS.map((task) => {
-    return { ...task };
-  });
+  const URL = 'https://task-list-api-c17.herokuapp.com/tasks';
 
-  const [tasks, setTasks] = useState(taskCopy);
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const tasklist = response.data.map((task) => {
+          return {
+            description: task.description,
+            id: task.id,
+            title: task.title,
+            isComplete: task.is_complete,
+          };
+        });
+        setTasks(tasklist);
+        console.log(tasklist);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const [tasks, setTasks] = useState([]);
 
   const toggleComplete = (taskId) => {
     const updatedTasks = [];
+    // axios.patch(`${URL}/${taskId}`);
+
     for (const task of tasks) {
       if (task.id !== taskId) {
         updatedTasks.push(task);
       } else {
-        const newTask = { ...task, isComplete: !task.isComplete };
-        updatedTasks.push(newTask);
+        const completeStatus = task.isComplete;
+        if (completeStatus === true) {
+          axios.patch(`${URL}/${taskId}/mark_incomplete`).then(() => {
+            const newTask = { ...task, isComplete: false };
+            updatedTasks.push(newTask);
+          });
+        } else if (completeStatus === false) {
+          axios.patch(`${URL}/${taskId}/mark_complete`).then(() => {
+            const newTask = { ...task, isComplete: true };
+            updatedTasks.push(newTask);
+          });
+        }
+        // const newTask = { ...task, isComplete: !task.isComplete };
+        // updatedTasks.push(newTask);
       }
     }
     setTasks(updatedTasks);
   };
 
   const deleteTask = (taskId) => {
-    const updatedTasks = [];
-    for (const task of tasks) {
-      if (task.id !== taskId) {
-        updatedTasks.push(task);
-      }
-    }
-    setTasks(updatedTasks);
+    axios
+      .delete(`${URL}/${taskId}`)
+      .then(() => {
+        const updatedTasks = [];
+        for (const task of tasks) {
+          if (task.id !== taskId) {
+            updatedTasks.push(task);
+          }
+        }
+        setTasks(updatedTasks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
